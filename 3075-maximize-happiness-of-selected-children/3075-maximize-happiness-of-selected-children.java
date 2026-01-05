745bffe33500334015d9d0069fc4a4022cfe35cd
Prahteek Gaba <prathu031105@gmail.com>
2026-01-05 23:44:43 +0530

    Time: 42 ms (80.28%), Space: 109.6 MB (18%) - LeetHub

--- 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.java
+++ 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.java
@@ -9,3 +9,25 @@ class Solution {
         return sum;
     }
 }
+/*
+
+class Solution {
+    public long maximumHappinessSum(int[] happiness, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int i = 0; i < k; i++){
+            pq.add(happiness[i]);
+        }
+        for(int i = k; i < happiness.length; i++){
+            pq.add(happiness[i]);
+            pq.poll();
+        }
+        long sum = 0;
+        int itr = k;
+        while(!pq.isEmpty()){
+            itr--;
+            sum += Math.max((pq.poll() - itr), 0);
+        }
+        return sum;
+    }
+}
+*/
