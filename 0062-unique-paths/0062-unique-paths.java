d5c89f6229058494db8f2aec8169809c20682574
Prahteek Gaba <prathu031105@gmail.com>
2026-01-16 23:51:22 +0530

    Time: 1 ms (15.09%), Space: 41.9 MB (92.89%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,5 +1,6 @@
 class Solution {
-    static int[][] dp;
+    // static int[][] dp;
+    /*
     static int pathss(int i, int j, int m, int n){
         if(i >= m || j >= n) return 0;
         if(dp[i][j] != -1) return dp[i][j];
@@ -9,9 +10,27 @@ class Solution {
         dp[i][j] = pathss(i + 1, j, m, n) + pathss(i, j + 1, m, n);
         return dp[i][j];
     }
+    */
     public int uniquePaths(int m, int n) {
-        dp = new int[m][n];
+        int[][] dp = new int[m][n];
+        /*
         for(int i = 0; i < m; i++) Arrays.fill(dp[i], -1);
         return pathss(0, 0, m, n);
+        */
+        dp[0][0] = 1;
+        
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(i == 0 && j == 0) continue;
+                dp[i][j] = 0;
+                if(j != 0){
+                    dp[i][j] += dp[i][j - 1];
+                }
+                if(i != 0){
+                    dp[i][j] += dp[i - 1][j];
+                }
+            }
+        }
+        return dp[m - 1][n - 1];
     }
 }
