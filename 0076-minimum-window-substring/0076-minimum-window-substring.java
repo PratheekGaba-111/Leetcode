48ead47c4232ea0c26c789c04613b3b3c49cd59e
Prahteek Gaba <prathu031105@gmail.com>
2026-02-10 19:06:29 +0530

    Time: 2 ms (99.63%), Space: 45.8 MB (74.76%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -0,0 +1,29 @@
+class Solution {
+    public String minWindow(String s, String t) {
+        int[] mps = new int[128];
+        int[] mpt = new int[128];
+        for(char c : t.toCharArray()) mpt[c]++;
+        int l = 0, r = 0, n = s.length();
+        int p1 = 0, p2 = 0, maxLen = n + 1;
+        int count = 0;
+        while(r < n){
+            char y = s.charAt(r);
+            mps[y]++;
+            if(mps[y] <= mpt[y]) count++;
+            while(l <= r && count >= t.length()){
+                char x = s.charAt(l);
+                mps[x]--;
+                if(mps[x] < mpt[x]) count--;
+                if((r - l + 1) < maxLen){
+                    maxLen = r - l + 1;
+                    p1 = l; p2 = r;
+                }
+                l++;
+            }
+            r++;
+        }
+        if(maxLen == n + 1) return "";
+        return s.substring(p1, p2 + 1);
+
+    }
+}
