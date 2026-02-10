6a08a48f43c50a586588d61e914994cb0a26b5b9
Prahteek Gaba <prathu031105@gmail.com>
2026-02-10 19:10:17 +0530

    Time: 2 ms (99.63%), Space: 45.2 MB (98.73%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,19 +1,19 @@
 class Solution {
     public String minWindow(String s, String t) {
         int[] mps = new int[128];
-        int[] mpt = new int[128];
-        for(char c : t.toCharArray()) mpt[c]++;
+        // int[] mpt = new int[128];
+        for(char c : t.toCharArray()) mps[c]++;
         int l = 0, r = 0, n = s.length();
         int p1 = 0, p2 = 0, maxLen = n + 1;
         int count = 0;
         while(r < n){
             char y = s.charAt(r);
-            mps[y]++;
-            if(mps[y] <= mpt[y]) count++;
+            mps[y]--;
+            if(mps[y] >= 0) count++;
             while(l <= r && count >= t.length()){
                 char x = s.charAt(l);
-                mps[x]--;
-                if(mps[x] < mpt[x]) count--;
+                mps[x]++;
+                if(mps[x] > 0) count--;
                 if((r - l + 1) < maxLen){
                     maxLen = r - l + 1;
                     p1 = l; p2 = r;
