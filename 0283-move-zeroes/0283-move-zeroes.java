0a5ed89a0b89eb19956b3e5b5d26d32324209462
Prahteek Gaba <prathu031105@gmail.com>
2026-02-21 00:17:36 +0530

    Time: 4 ms (10.57%), Space: 47.7 MB (76.33%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -3,16 +3,36 @@ class Solution {
         int n = nums.length;
         int j = 0;
         for(int i = 0; i < n; i++){
-            j = i + 1;
+            j = Math.max(i, j);
             if(nums[i] == 0){
-                while(j < n && nums[j] == 0) j++;
-                if(j < n){
+                // j++;
+                boolean moved = false;
+                while(j < n && nums[j] == 0){
+                    j++;
+                    moved = true;
+                }
+                // System.out.println(moved + " " + j);
+                if(moved && j < n){
                     nums[i] = nums[i] ^ nums[j];
                     nums[j] = nums[i] ^ nums[j];
                     nums[i] = nums[i] ^ nums[j];
                 }
             }
-            if(j >= n) break;
+            // if(j >= n) break;
         }
     }
+    /*
+    we know that
+        0 1 0 3 12
+
+        op 
+        1 3 12 0 0
+
+        but
+        lets say l = 0, r = 0
+        we need to fin r pointer till next non zero value
+
+
+
+    */
 }
