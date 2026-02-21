5eea9f9bc1683b5a6898981688510827e29312bb
Prahteek Gaba <prathu031105@gmail.com>
2026-02-21 22:53:52 +0530

    Time: 3 ms (31.92%), Space: 48.1 MB (42.02%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,22 +1,22 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        /*
-            from ith index we can jump to i + ar[i] indexes
-
-        */
-        if(nums.length == 1) return true;
-        // boolean[] dp = new boolean[nums.length];
+        // initially at first position
+        // so we can jump upto all 2 blocks right
+        // so we can make all the possible jumps from i + 1 to i + 2
+        // so all those indexes are reachable
+        // so lets say to reach last index
+        // we must need an index such that from that index coming to last index is possible
+        // so lets sayyyyyyyyyyyyy we start from 0 to n
+        int n = nums.length;
+        if(n == 1) return true;
+        boolean[] dp = new boolean[n];
         int val = nums[0];
-        for(int i = 1; i < nums.length; i++){
+        for(int i = 0; i < n; i++){
             if(val != 0){
-                // dp[i] = true;
-                val--;
-                val = Math.max(val, nums[i]);
-            }
-            else{
-                return false;
+                dp[i] = true;
+                val = Math.max(val - 1, nums[i]);
             }
         }
-        return true;
+        return dp[n - 1];
     }
 }
