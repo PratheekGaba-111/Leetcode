93caf6df74e71da7661dc25a9e80fc9fc19fb994
Prahteek Gaba <prathu031105@gmail.com>
2026-02-23 19:34:16 +0530

    Time: 2 ms (90.13%), Space: 47.2 MB (98.93%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,35 +1,19 @@
 class Solution {
     public void moveZeroes(int[] nums) {
         int n = nums.length;
-        int j = 0;
-        for(int i = 0; i < n; i++){
-            j = Math.max(i, j);
-            if(nums[i] == 0){
-                // j++;
-                while(j < n && nums[j] == 0){
-                    j++;
-                }
-                if(j < n){
-                    nums[i] = nums[i] ^ nums[j];
-                    nums[j] = nums[i] ^ nums[j];
-                    nums[i] = nums[i] ^ nums[j];
-                }
+        int l = 0, r = 0;
+        while(r < n){
+            // we swap l and r if r is nonzero
+            if(nums[r] != 0){
+                // swap l and r
+                //swap
+                int t = nums[l];
+                nums[l] = nums[r];
+                nums[r] = t;
+                l++;
             }
-            if(j >= n) break;
+            r++;
         }
+        return;
     }
-    /*
-    we know that
-        0 1 0 3 12
-
-        op 
-        1 3 12 0 0
-
-        but
-        lets say l = 0, r = 0
-        we need to fin r pointer till next non zero value
-
-
-
-    */
 }
