6c3bc49c5930f47461c0ef3f8cb55f6159b32cbb
Prahteek Gaba <prathu031105@gmail.com>
2026-02-24 14:26:14 +0530

    Time: 0 ms (100%), Space: 43.6 MB (92.96%) - LeetHub

--- 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.java
+++ 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.java
@@ -15,20 +15,19 @@
  */
 class Solution {
     static int ans;
-    public static void generateAns(TreeNode root, int val){
-        if(root == null) return;
-        val = (val << 1) | root.val;
+    public static void generate(TreeNode root, int sum){
         if(root.left == null && root.right == null){
-            ans += val;
+            sum <<= 1;
+            sum |= root.val;
+            ans += sum;
             return;
         }
-        generateAns(root.left, val);
-        generateAns(root.right, val);
+        if(root.left != null) generate(root.left, sum << 1 | root.val);
+        if(root.right != null) generate(root.right, sum << 1 | root.val);
     }
-
     public int sumRootToLeaf(TreeNode root) {
         ans = 0;
-        generateAns(root, 0);
+        generate(root, 0);
         return ans;
     }
 }
